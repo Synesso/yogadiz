6a285ba465b368664402c8368749360893fbf0d6
Jem Mawson <jem.mawson@gmail.com>
2014-03-12 19:52:39 +1100

    first build

---
+++ js/super-image.js
@@ -0,0 +1,8 @@
+jQuery(function($){ 
+  $.supersized({ 
+    slides  :  	[ {
+      image : 'img/yoga-teacher-blackburn.JPG', title : 'Yoga Teacher Melbourne East'
+    } ] ,
+    vertical_center : 1
+  }); 
+});
