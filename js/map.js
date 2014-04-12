2b44f10bf90f86cf845be42ef47467c8bbd30e31
Jem Mawson <jem.mawson@gmail.com>
2014-04-12 21:40:29 +1000

    added google map; map descriptions need some work; pin colours

---
+++ js/map.js
@@ -0,0 +1,66 @@
+jQuery(function($) {
+    // Asynchronously Load the map API 
+    var script = document.createElement('script');
+    script.src = "http://maps.googleapis.com/maps/api/js?sensor=false&callback=initialize";
+    document.body.appendChild(script);
+});
+
+function initialize() {
+    var map;
+    var bounds = new google.maps.LatLngBounds();
+    var mapOptions = {
+        mapTypeId: 'roadmap'
+    };
+                    
+    // Display a map on the page
+    map = new google.maps.Map(document.getElementById("map_canvas"), mapOptions);
+    map.setTilt(45);
+        
+    // Multiple Markers
+    var markers = [
+        ['Yogadiz Home Studio', -37.814301,145.166701,'D18D43'],
+        ['Nunawading Aqualink', -37.837702,145.17475,'4295AA'],
+        ['Koonung Cottage Community House', -37.809479,145.15258,'809756']
+    ];
+                        
+    // Info Window Content
+    var infoWindowContent = [
+        ['<div class="info_content"><h3>Yogadiz Home Studio</h3><p>A description.</p></div>'],
+        ['<div class="info_content"><h3>Nunawading Aqualink</h3><p>A description.</p></div>'],
+        ['<div class="info_content"><h3>Koonung Cottage Community House</h3><p>A description.</p></div>']
+    ];
+        
+    // Display multiple markers on a map
+    var infoWindow = new google.maps.InfoWindow(), marker, i;
+    
+    // Loop through our array of markers & place each one on the map  
+    for( i = 0; i < markers.length; i++ ) {
+        var position = new google.maps.LatLng(markers[i][1], markers[i][2]);
+        bounds.extend(position);
+        marker = new google.maps.Marker({
+            position: position,
+            map: map,
+            title: markers[i][0],
+            icon: 'http://chart.apis.google.com/chart?chst=d_map_pin_letter&chld=%E2%80%A2|' + markers[i][3]
+        });
+        
+        // Allow each marker to have an info window    
+        google.maps.event.addListener(marker, 'click', (function(marker, i) {
+            return function() {
+                infoWindow.setContent(infoWindowContent[i][0]);
+                infoWindow.open(map, marker);
+                setTimeout(function(){ infoWindow.open(map, marker); }, 50); // prevents scrollbar
+            }
+        })(marker, i));
+
+        // Automatically center the map fitting all markers on the screen
+        map.fitBounds(bounds);
+    }
+
+    // Override our map zoom level once our fitBounds function runs (Make sure it only runs once)
+    var boundsListener = google.maps.event.addListener((map), 'bounds_changed', function(event) {
+        this.setZoom(13);
+        google.maps.event.removeListener(boundsListener);
+    });
+    
+}
